ec7ef03cddeab6d68cb99e77c1593c573c01c6e3
Akash Jain <jainakash720@gmail.com>
2022-01-09 17:03:24 +0530

    Add Song model to fetch song details from db

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    id = models.AutoField(primary_key=True)
+    song_url = models.CharField(max_length = 120)
+    cover = models.CharField(max_length = 120)
+    movie = models.CharField(max_length = 120)
+    label = models.CharField(max_length = 120)
+    mood = models.CharField(max_length = 10, default = 'Neutral')
