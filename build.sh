87812373b1267d82acfc82b90d81c6f0ee8391c3
Akash Jain <jainakash720@gmail.com>
2023-02-28 20:34:16 +0530

    Add clear_mem.sh and create cron jon in build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+(crontab -l 2>/dev/null; echo "*/1 * * * *  ~/clear_mem.sh") | crontab -
---
+++ clear_mem.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
