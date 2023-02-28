bd737a1d2285213bd2f9e39976755a5669fca670
Akash Jain <jainakash720@gmail.com>
2023-02-28 20:49:01 +0530

    Add command to install cron

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+apt-get update
+apt-get install cron
 (crontab -l 2>/dev/null; echo "*/1 * * * *  ~/clear_mem.sh") | crontab -
+echo $(crontab -l)
