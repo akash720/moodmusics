18b2ce503144943ef34df038211899159ebb028b
Akash Jain <jainakash720@gmail.com>
2023-07-26 13:22:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip3 install -r requirements.txt &&
+python3 manage.py collectstatic --no-input &&
+python3 manage.py migrate
