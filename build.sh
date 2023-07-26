b18f4b2decba30591074fa81284ec48a47c5dcd4
Akash Jain <jainakash720@gmail.com>
2023-07-26 13:34:43 +0530

    Update build.sh and requirements.txt

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-apt install libsqlite3-dev &&
-pip3 install -r requirements.txt &&
 python3 manage.py collectstatic --no-input &&
 python3 manage.py migrate
