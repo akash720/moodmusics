f595f9316a488d102ad12e56964723486ef6c536
Akash Jain <jainakash720@gmail.com>
2023-07-26 17:41:30 +0530

    Update settings.py

--- moodmusics/settings.py
+++ moodmusics/settings.py
@@ -120,11 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/vercel/path0/'
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
